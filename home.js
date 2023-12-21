21c55226ae2ac12547e315b7f66efadd31b70aa6
DavideTarantino <davidetarantino2005@gmail.com>
2023-12-21 15:25:22 +0100

    fine dell'esercizio

---
+++ home.js
@@ -0,0 +1,7 @@
+let NomeUtente = prompt("Scrvi il tuo nome");
+
+let CognomeUtente = prompt("Scrivi il tuo cognome");
+
+let ColoreUtente = prompt("Scrivi il tuo colore preferito");
+
+document. writeln("<h3> Ciao, la password creata per te è:" +" " +NomeUtente +CognomeUtente +ColoreUtente +23 +"</h3>");
